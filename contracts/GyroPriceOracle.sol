0d2aa24f75171b85538928abb920d7c630bfade7
Ariah Klages-Mundt <aklamun@users.noreply.github.com>
2021-01-17 14:04:30 +0000

    add getBptPrice

--- contracts/GyroPriceOracle.sol
+++ contracts/GyroPriceOracle.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.7.0;
 
 import "hardhat/console.sol";
+import "./abdk/ABDKMath64x64.sol";
 
 interface GyroPriceOracle {
     function getAmountToMint(
@@ -13,6 +14,11 @@ interface GyroPriceOracle {
         uint256 _gyroAmount,
         address[] memory _tokensOut
     ) external view returns (uint256[] memory _amountsOut);
+
+    function getBptPrice(
+        address _bptAddress,
+        uint256[] _underlyingPrices
+    ) external view returns (uint256 _bptPrice);
 }
 
 contract DummyGyroPriceOracle is GyroPriceOracle {
@@ -36,5 +42,32 @@ contract DummyGyroPriceOracle is GyroPriceOracle {
             amounts[i] = _gyroAmount / _tokensOut.length;
         }
         return amounts;
+    }
+
+    function getBptPrice(
+        address _bptAddress,
+        uint256[] _underlyingPrices
+    ) external view returns (uint256 _bptPrice) {
+        /* TODO:
+            bptSupply = # of BPT tokens
+            bpWeights = array of pool weights (require _underlyingPrices comes in same order)
+            k = constant = product of reserves^weight
+            bptPrice = (k * product of (p_i / w_i)^w_i ) / btpSupply
+
+            functions from ABDKMath64x64 library
+            -- exp_2 = binary exponent
+            -- log_2 = binary logarithm
+            -- mul = calculate x*y
+
+            x^y = 2^(y log_2 x)
+            exp_2( mul(y, log_2(x)) )
+        */
+        uint256 bptSupply;
+        uint256[] bpWeights;
+        uint256 _k = 1;
+        for (uint256 i=0; i < bpWeights.length; i++) {
+            _k = mustMulExp(_k, )
+        }
+
     }
 }
