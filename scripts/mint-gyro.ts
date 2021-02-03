c4d33450faad56daad4a3b9af42be6dfc47b3a24
Daniel Perez <daniel@perez.sh>
2021-02-03 18:20:57 +0000

    Update minting script

--- scripts/mint-gyro.ts
+++ scripts/mint-gyro.ts
@@ -1,26 +1,30 @@
-import BN from "bn.js";
+import { BigNumber } from "ethers";
 import hre from "hardhat";
 import { BPool__factory as BPoolFactory } from "../typechain/factories/BPool__factory";
 import { GyroFund__factory as GyroFundFactory } from "../typechain/factories/GyroFund__factory";
 const { deployments, ethers } = hre;
 
 const amountToMint = 100;
+const poolNames = ["usdc_weth", "weth_dai"];
 
 async function main() {
   const [account] = await ethers.getSigners();
-  const wethDaiPoolDeployment = await deployments.get("BPoolweth_dai");
   const gyroFundDeployment = await deployments.get("GyroFundV1");
   const gyroFund = GyroFundFactory.connect(gyroFundDeployment.address, account);
-  const wethDaiPool = BPoolFactory.connect(wethDaiPoolDeployment.address, account);
+  const poolsDeployments = await Promise.all(poolNames.map((p) => deployments.get(`BPool${p}`)));
+  const poolAddresses = poolsDeployments.map((p) => p.address);
 
-  const ten = new BN(10).pow(new BN(19)).toString();
-  const mintedAmount = new BN(10).pow(new BN(18)).mul(new BN(amountToMint)).toString();
-  await wethDaiPool.approve(gyroFund.address, ten);
-  const tx = await gyroFund.mintTest([wethDaiPool.address], [ten], mintedAmount);
+  const ten = BigNumber.from(10).pow(BigNumber.from(19));
+  await Promise.all(
+    poolAddresses.map((a) => BPoolFactory.connect(a, account).approve(gyroFund.address, ten))
+  );
+
+  const mintedAmount = BigNumber.from(10).pow(18).mul(amountToMint);
+  const tx = await gyroFund.mintTest(poolAddresses, [ten, ten], mintedAmount);
   await tx.wait();
 
   const balance = await gyroFund.balanceOf(account.address);
-  const readableBalance = balance.div(new BN(10).pow(new BN(18)).toString());
+  const readableBalance = balance.div(BigNumber.from(10).pow(18)).toString();
   console.log(`GyroFund balance: ${readableBalance}`);
 }
 
